b4dc829e148d74ebd619228074746cbd2313415d
IngisRedd <iarojas2@uc.cl>
2021-09-15 18:17:08 -0300

    can read command and file input

--- T2/src/scheduler/main.c
+++ T2/src/scheduler/main.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include "structs.h"
 #include "../file_manager/manager.h"
 
@@ -6,19 +9,65 @@ int main(int argc, char **argv)
 {
   printf("Hello T2!\n");
 
-  InputFile *file = read_file("input.txt");
+  if (argc != 3 && argc != 4) {
+    printf("Expected 3 or 4 arguments, recieved %d\n", argc);
+    return 0;
+  }
 
-  printf("Reading file of length %i:\n", file->len);
-  for (int i = 0; i < file->len; i++)
-  {
-    char **line = file->lines[i];
-    printf(
-        "\tProcess %s from factory %s has init time of %s and %s bursts.\n",
-        line[0], line[2], line[1], line[3]);
-        printf("Fabrica: %d\n", (int)line[2][8]);
-    char* nombre = line[0];
-    int nFabrica = line[2];
-    Process* process = process_init(i, nombre, nFabrica);
-    printf("Process %s fabrica %d\n", process->name, process->nFabrica);
+  // Obtain input from command line
+  char *filename = argv[1];
+  char *outputname = argv[2];
+  // Code from https://stackoverflow.com/questions/10347689/how-can-i-check-whether-a-string-ends-with-csv-in-c
+  char *dot = strrchr(outputname, '.');
+  if (!(dot && !strcmp(dot, ".csv"))) {
+    printf("Output filename must end with .csv\n");
+    return 0;
+  }
+  
+  int Q = 100;
+  if (argc == 4) {
+    Q = atoi(argv[3]);
+  }
+
+
+  if ( access( filename, F_OK ) == 0 ) {
+    // if file exists, read input file
+    InputFile *file = read_file(filename);
+
+    printf("Reading file of length %i:\n", file->len);
+    for (int i = 0; i < file->len; i++)
+    {
+      char **line = file->lines[i];
+      printf(
+          "\tProcess %s from factory %s has init time of %s and %s bursts.\n",
+          line[0], line[2], line[1], line[3]);
+          printf("Fabrica: %d\n", (int)line[2][8]);
+      // char* nombre = line[0];
+      // int nFabrica = line[2];
+      // Process* process = process_init(i, nombre, nFabrica);
+      // printf("Process %s fabrica %d\n", process->name, process->nFabrica);
+    }
+    // Free input file memory:
+    input_file_destroy(file);
+
+    // Initialize Queue:
+
+    // SIMULATION START!!
+
+
+
+
+
+    // Create output file
+    FILE *fp; 
+    fp  = fopen (outputname, "w");
+    if (fp != NULL) {
+      fprintf(fp, "Aqui va el output \n");
+    }
+    fclose (fp);
+  
+  } else {
+    // if input file doesn't exist
+    printf("That input file does not exist\n");
   }
 }
--- T2/src/scheduler/structs.c
+++ T2/src/scheduler/structs.c
@@ -4,65 +4,65 @@
 #include <string.h>
 #include <stdbool.h>
 
-//######## PROCESS ############
+// //######## PROCESS ############
 
-Process* process_init(int id, char* name, int nFabrica){
-    Process* process = calloc(1, sizeof(Process));
-    process -> id = id;
-    process -> name[255] = name;
-    process -> nFabrica = nFabrica;
-    process -> estado = 0;
+// Process* process_init(int id, char* name, int nFabrica){
+//     Process* process = calloc(1, sizeof(Process));
+//     process -> id = id;
+//     process -> name[255] = name;
+//     process -> nFabrica = nFabrica;
+//     process -> estado = 0;
 
-    return process;
-}
-void process_destroy(Process* process){
-    free(process);
-}
+//     return process;
+// }
+// void process_destroy(Process* process){
+//     free(process);
+// }
 
-//######## QUEUE ############
+// //######## QUEUE ############
 
-Queue* queue_init(){
-    Queue* queue = calloc(1, sizeof(Queue));
-    queue -> line = calloc(8, sizeof(Process*));
-    queue -> cant_process = 0;
-}
-void queue_destroy(Queue* queue){
-    for (int p=0; p<queue->cant_process;p++){
-        process_destroy(queue->line[p]);
-    }
-    free(queue);
-}
+// Queue* queue_init(){
+//     Queue* queue = calloc(1, sizeof(Queue));
+//     queue -> line = calloc(8, sizeof(Process*));
+//     queue -> cant_process = 0;
+// }
+// void queue_destroy(Queue* queue){
+//     for (int p=0; p<queue->cant_process;p++){
+//         process_destroy(queue->line[p]);
+//     }
+//     free(queue);
+// }
 
-//######## Auxiliares #########
+// //######## Auxiliares #########
 
-int f_calculator(Queue* queue){
-    int* fabricas[queue->cant_process];
-    int fabs = 0;
-    int in = 0;
-    int f = 0;
-    for (int p=0; p<queue->cant_process; p++){
-        for (int f=0; f<queue->cant_process;f++){
-            if (fabricas[f] == queue->line[p]->nFabrica){
-                in = 1;
-            }
-        }
-        if (in == 0){
-            fabricas[fabs]=p;
-            fabs+=1;
-        } else {
-            f+=1;
-        }
-    }
-    return f;
-}
+// int f_calculator(Queue* queue){
+//     int* fabricas[queue->cant_process];
+//     int fabs = 0;
+//     int in = 0;
+//     int f = 0;
+//     for (int p=0; p<queue->cant_process; p++){
+//         for (int f=0; f<queue->cant_process;f++){
+//             if (fabricas[f] == queue->line[p]->nFabrica){
+//                 in = 1;
+//             }
+//         }
+//         if (in == 0){
+//             fabricas[fabs]=p;
+//             fabs+=1;
+//         } else {
+//             f+=1;
+//         }
+//     }
+//     return f;
+// }
 
-int qi_calculator(int Q, Queue* queue, int fabrica, int f){
-    int ni = 0;
-    for (int p=0; p<queue->cant_process; p++){
-        if (queue->line[p]->nFabrica == fabrica){
-            ni+=1;
-        }
-    }
-    int qi = Q/(ni*f);
-    return qi;
-}
+// int qi_calculator(int Q, Queue* queue, int fabrica, int f){
+//     int ni = 0;
+//     for (int p=0; p<queue->cant_process; p++){
+//         if (queue->line[p]->nFabrica == fabrica){
+//             ni+=1;
+//         }
+//     }
+//     int qi = Q/(ni*f);
+//     return qi;
+// }
